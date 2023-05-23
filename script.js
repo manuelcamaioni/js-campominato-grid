e3e4d3d123391fc07bb1833308908ccfcc71c175
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-23 14:42:37 +0200

    added function and edited page style

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ btnElement.addEventListener('click', function(){
     const gridElement = document.getElementById('grid');
     gridElement.classList.toggle('grid');
 });
+
+function createChildElements (tagName, className){
+    const childElement = document.createElement(tagName);
+    childElement.classList.add(className);
+}
