5b524787e9d1956e279272fbd7d443eec352e60a
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-23 17:47:46 +0200

    added comments

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const btnElement = document.getElementById('play-button');
 const mainElement = document.querySelector('main');
-const gridElement = getElement('div', 'grid');
+const gridElement = getElement('div', 'grid'); // creates a div with class='grid'
+
 btnElement.addEventListener('click', function(){
     
     mainElement.appendChild(gridElement);
   
-        for(let i = 0; i < 100; i++){
+        for(let i = 0; i < 100; i++){ // creates 100 cells displayed in the div.grid parent
             const divElement = getElement('div', 'cell');
             divElement.innerHTML += i + 1;
             gridElement.appendChild(divElement);
@@ -21,10 +22,17 @@ btnElement.addEventListener('click', function(){
                 }
                 
             });
-        
     }
 });
 
+/**
+ * 
+ * Creates a DOM element
+ * 
+ * @param {string} tagName // write a string, it'll be implemented as a Node in html
+ * @param {string} className  // write a class you want your tag to have
+ * @returns a html node with given class
+ */
 function getElement(tagName, className){
     const childElement = document.createElement(tagName);
     childElement.classList.add(className);
