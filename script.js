58036646f6ed829dd90c4b494ef5086bacda96b4
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-23 15:20:22 +0200

    added cells to grid

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ const gridElement = document.getElementById('grid');
 
 
 btnElement.addEventListener('click', function(){
+    
     gridElement.classList.toggle('grid');
 
     for(let i = 0; i<100; i++){
 
         const divElement = getElements('div', 'cell');
-
+        divElement.innerHTML += i + 1;
         gridElement.appendChild(divElement);
 
     }
 });
 
 
-
-
-
 function getElements(tagName, className){
     const childElement = document.createElement(tagName);
     childElement.classList.add(className);
+
+    return childElement;
 }
