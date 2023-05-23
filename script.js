60bf80750e78ddc7458fa167282ca0b059ff4672
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-23 17:12:58 +0200

    update

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 const btnElement = document.getElementById('play-button');
-const gridElement = document.getElementById('grid');
 
+const mainElement = document.querySelector('main');
+
+const gridElement = getElement('div', 'grid');
 
 
 btnElement.addEventListener('click', function(){
-    
-    gridElement.classList.toggle('grid');
-    if(gridElement.classList.contains('grid')){
+    mainElement.appendChild(gridElement);
+  
         for(let i = 0; i < 100; i++){
-
-            const divElement = getElements('div', 'cell');
+            const divElement = getElement('div', 'cell');
             divElement.innerHTML += i + 1;
             gridElement.appendChild(divElement);
 
             divElement.addEventListener('click', function(){
-                divElement.classList.toggle('active');
-                console.log(divElement.innerHTML);
+                divElement.classList.add('active');
+                console.log(parseInt(divElement.innerHTML));
             });
         
-        }
-    }else{
-        gridElement.innerHTML = '';
     }
-    
 });
 
 
-function getElements(tagName, className){
+function getElement(tagName, className){
     const childElement = document.createElement(tagName);
     childElement.classList.add(className);
 
